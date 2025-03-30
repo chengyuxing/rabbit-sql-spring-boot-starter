82f91d7d29f2f5c3f9dd92786705d20ad7a575f6
chengyuxing <chengyuxingo@gmail.com>
2025-03-30 14:03:38 +0800

    4.0.4

--- src/main/java/com/github/chengyuxing/sql/spring/autoconfigure/BakiAutoConfiguration.java
+++ src/main/java/com/github/chengyuxing/sql/spring/autoconfigure/BakiAutoConfiguration.java
@@ -4,7 +4,9 @@ import com.github.chengyuxing.sql.Baki;
 import com.github.chengyuxing.sql.XQLFileManager;
 import com.github.chengyuxing.sql.spring.properties.*;
 import com.github.chengyuxing.sql.spring.utils.BeanUtil;
+
 import jakarta.annotation.PostConstruct;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
