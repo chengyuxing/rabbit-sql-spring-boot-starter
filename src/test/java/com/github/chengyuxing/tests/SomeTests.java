f5c32c59496af0e9219835478fe299920f0abdff
chengyuxing <chengyuxingo@gmail.com>
2025-09-17 22:43:13 +0800

    tests

--- src/test/java/com/github/chengyuxing/tests/SomeTests.java
+++ src/test/java/com/github/chengyuxing/tests/SomeTests.java
@@ -1,18 +1,10 @@
 package com.github.chengyuxing.tests;
 
-import com.github.chengyuxing.sql.EntityManager;
-import org.junit.Test;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
 public class SomeTests {
-    @Test
-    public void test1() {
-        EntityManager entityManager = new EntityManager(':');
-        System.out.println(entityManager.getEntityMeta(Me.class));
-    }
-
     @Entity
     public class Me {
 
